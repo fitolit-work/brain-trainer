4ad40f3758aa8c6317005260ff1e840e8aa27f21
Alex <fitolit@protonmail.com>
2024-10-30 08:14:30 +0300

    Added a question for the player and replaced time.sleep with input

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-import time
 from model import db_func as db
+from utils import checks
 
 db.make_db()
 
 print(
     'Hello! This is a program to train your brain. Improve your mental arithmetic, logic, memory and speed of thought')
-time.sleep(2)
+input('Press <Enter>')
+
+while True:
+    user_enter = checks.checking_numbers('Are you playing for the first time?',
+                                         'Enter the correct value',
+                                         'Enter 1 if YES first.\nEnter 2 if NO has already played\nYour answer: ')
+    if user_enter == 1 or user_enter == 2: break
+
 print('Select training direction: ')
-time.sleep(2)
+input('Press <Enter>')
 print('Enter 1 - to practice mental counting')
-time.sleep(1)
 print('Enter 2 - to train the speed of thought')
-time.sleep(1)
+
 training_direction = input('Enter the number 1 or 2: ')
 
 if training_direction == '1':
--- utils/checks.py
+++ utils/checks.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 def spaces_in_words(request_text: str, error_text: str, output_text: str) -> str:
     """
         Checks for whitespace characters in the input string
@@ -8,12 +11,27 @@ def spaces_in_words(request_text: str, error_text: str, output_text: str) -> str
     """
     while True:
         try:
-            login = input(f'{request_text} ').strip()
+            user_text = input(f'{request_text} ').strip()
         except KeyboardInterrupt:
             print('\nYou are out of the game')
             exit()
-        if ' ' in login:
+        if ' ' in user_text:
             print(error_text)
         else:
-            print(output_text, login)
-            return login
+            print(output_text, user_text)
+            return user_text
+
+
+def checking_numbers(welcome_text: str, error_text: str, *args: str) -> tuple[int, ...]:
+    while True:
+        print(f'{welcome_text} ')
+        try:
+            nums_tuple = tuple([int(input(f'{num_text} ')) for num_text in args])
+            break
+        except KeyboardInterrupt:
+            print('\nВы вышли из игры')
+            exit()
+        except ValueError:
+            print(f'{error_text} ')
+
+    return nums_tuple
